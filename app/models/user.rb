2bc22de0b1c48d27c8a8a41406991863bb87b588
Kamil Kapturski <kamil3392@o2.pl>
2015-05-20 19:48:05 +0000

    creating scaffold Project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   
-  validates :first_name,
+  validates :firs_name,
   presence: true
   validates :last_name,
   presence: true
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
     }
   
   def to_s
-    "#{first_name} #{last_name} "
+    "#{firs_name} #{last_name} "
   end
   
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150520121103) do
+ActiveRecord::Schema.define(version: 20150520194708) do
+
+  create_table "projects", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "projects", ["user_id"], name: "index_projects_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "firs_name"
