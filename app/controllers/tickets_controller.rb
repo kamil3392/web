05b33422d98deef33b5428348a1d2fe84e85ca3d
Kamil Kapturski <kamil3392@o2.pl>
2015-06-01 11:35:10 +0000

    adding method destroy tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -9,7 +9,15 @@ def index
      @ticket = @project.tickets.create(ticket_params)
      redirect_to project_path(@project)
   end
- 
+  
+ def destroy
+   @project = Project.find(params[:project_id])
+   @ticket = @project.tickets.find(params[:id])
+   @ticket.destroy
+   redirect_to project_path(@project)
+  end
+
+  
   private
   def ticket_params
     params.require(:ticket).permit(:title,:comment,:priority,:rank,:status)
