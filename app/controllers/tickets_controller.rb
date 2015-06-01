8c2a069bb186e63226fef44b69946f32bc80fb4e
Kamil Kapturski <kamil3392@o2.pl>
2015-06-01 13:18:55 +0000

    add method edit ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,7 +1,14 @@
 class TicketsController < ApplicationController
+def show
+    @project= Project.find(params[:project_id])
+  @ticket=@project.tickets.find(params[:project_id])
+
+  end
 def index
-  @project = Project.find(params[:project_id])
- end
+      @project= Project.find(params[:project_id])
+      @tickets=@project.tickets.all
+    
+  end
   def new
   end
    def create
@@ -16,8 +23,22 @@ def index
    @ticket.destroy
    redirect_to project_path(@project)
   end
-
+  def edit
+    @project= Project.find(params[:project_id])
+    @ticket=@project.tickets.find(params[:id])
+   
+  end
   
+  def update
+    @project= Project.find(params[:project_id])
+    @ticket=@project.tickets.find(params[:id])
+ 
+    if @ticket.update(ticket_params)
+      redirect_to project_ticket_path(@project, @ticket)
+    else
+      render 'edit'
+    end
+  end  
   private
   def ticket_params
     params.require(:ticket).permit(:title,:comment,:priority,:rank,:status)
